7d99f49b2e92964e51cc8670253c5e2bc224ea8f
Leo <leodarkseid@gmail.com>
2023-09-05 11:48:01 +0100

    fix: added type URL to api_url

--- src/lazy/index.ts
+++ src/lazy/index.ts
@@ -3,7 +3,7 @@ export { checkVerification } from "./checkVerification";
 
 export interface ILazy_verify {
   api_key: string | undefined;
-  api_url: string;
+  api_url: URL;
   contractAddress: string;
   contractName: string;
   sourceCode: string;
